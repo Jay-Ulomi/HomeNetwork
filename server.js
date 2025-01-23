c25568a7b681237722e454299c85c4325a4727a2
Jay-Ulomi <jayulomi160@gmail.com>
2025-01-23 22:25:09 +0300

    Add npmignore and configuration files for various modules

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const multer = require('multer');
 const path = require('path'); 
 const PORT = 3030;
 
 // Import routes
 const homeRoutes = require('./routes/HomeRoutes');
+const contactRoutes = require('./routes/contactRoutes');
 
+
+// Middleware to parse form data
+const upload = multer();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
 app.set('view engine', 'ejs');
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(expressLayouts);
@@ -16,6 +26,7 @@ app.use(express.static('public'));
 
 // Use routes
 app.use('/', homeRoutes);
+app.use('/contact', contactRoutes);
 
 // Start the server
 app.listen(PORT, () => {
